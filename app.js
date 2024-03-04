22e85e58881f3e81a5418c2b1a97f86d6d9a5c80
sharif23pak <160602479+sharif23pak@users.noreply.github.com>
2024-03-04 22:42:07 +0000

    java assignenent 1

---
+++ app.js
@@ -0,0 +1,86 @@
+// var x = 10, y = "Hello", z = true;
+
+// var myVar;
+// var myVar1;
+// var _myVar;
+// var $myVar;
+// var myVar_name;
+
+// var 1myVar; // Cannot start with a number
+// var my Var; // Contains whitespace
+// var my-Var; // Contains a hyphen
+// var my@Var; // Contains special character '@'
+// var var; // Reserved keyword
+
+// var num1 = 10;
+//         var num2 = 5;
+//         var sum = num1 + num2;
+//         document.write("The result of addition is: " + sum)
+
+// var num1 = 10;
+// var num2 = 5;
+// var subtraction = num1 - num2;
+// var multiplication = num1 * num2;
+// var division = num1 / num2;
+// var modulus = num1 % num2;
+
+// document.write("Subtraction: " + subtraction + "<br>");
+// document.write("Multiplication: " + multiplication + "<br>");
+// document.write("Division: " + division + "<br>");
+// document.write("Modulus: " + modulus);
+
+// var num1 = 10;
+// var num2 = 5;
+// var subtraction = num1 - num2;
+// var multiplication = num1 * num2;
+// var division = num1 / num2;
+// var modulus = num1 % num2;
+
+// document.write("Subtraction: " + subtraction + "<br>");
+// document.write("Multiplication: " + multiplication + "<br>");
+// document.write("Division: " + division + "<br>");
+// document.write("Modulus: " + modulus);
+
+// var num; 
+// document.write("Value after variable declaration is: " + num + "<br>");
+// num = 5; ber
+// document.write("Initial value: " + num + "<br>");
+// num++; 
+// document.write("Value after increment is: " + num + "<br>");
+// num += 7; 
+// document.write("Value after adding 7 is: " + num);
+
+
+// var num;
+//         document.write("Value after variable declaration is: " + num + "<br>");
+//         num = 5;
+//         document.write("Initial value: " + num + "<br>");
+//         num++;
+//         document.write("Value after increment is: " + num + "<br>");
+//         num += 7;
+//         document.write("Value after adding 7 is: " + num + "<br>");
+//         num--;
+//         document.write("Value after decrement is: " + num + "<br>");
+//         var remainder = num % 3;
+//         document.write("The remainder after dividing by 3 is: " + remainder);
+
+
+  // // Prices of items and ordered quantities
+  // var priceItem1 = 25; // Price of item 1
+  // var priceItem2 = 30; // Price of item 2
+  // var quantityItem1 = 2; // Ordered quantity of item 1
+  // var quantityItem2 = 3; // Ordered quantity of item 2
+
+  // // Shipping charges
+  // var shippingCharges = 10;
+
+  // // Calculate total cost
+  // var totalCost = (priceItem1 * quantityItem1) + (priceItem2 * quantityItem2) + shippingCharges;
+
+  // // Display receipt
+  // document.write("<p>Price of Item 1: $" + priceItem1 + "</p>");
+  // document.write("<p>Price of Item 2: $" + priceItem2 + "</p>");
+  // document.write("<p>Ordered Quantity of Item 1: " + quantityItem1 + "</p>");
+  // document.write("<p>Ordered Quantity of Item 2: " + quantityItem2 + "</p>");
+  // document.write("<p>Shipping Charges: $" + shippingCharges + "</p>");
+  // document.write("<h3>Total Cost: $" + totalCost + "</h3>");
